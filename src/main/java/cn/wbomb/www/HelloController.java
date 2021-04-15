dc1c26e3637a783c27a9a40fe9ff7729bfe10157
georgeT <jobsteng@163.com>
2021-04-15 16:03:07 +0800

    update

---
+++ src/main/java/cn/wbomb/www/HelloController.java
@@ -0,0 +1,16 @@
+package cn.wbomb.www;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+
+
+}
