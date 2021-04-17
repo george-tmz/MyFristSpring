2c96ec99507034adc371aa8fe3acd4e6fa6d047c
George <jobsteng@163.com>
2021-04-18 00:51:46 +0800

    链接数据

--- src/main/java/cn/wbomb/www/mapper/UserMapper.java
+++ src/main/java/cn/wbomb/www/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package cn.wbomb.www.mapper;
 
 import cn.wbomb.www.entity.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -9,4 +10,11 @@ import org.apache.ibatis.annotations.Select;
 public interface UserMapper {
     @Select("select * from user where id = #{id}")
     User getUserById(@Param("id") Integer id);
+
+    @Insert("INSERT INTO `user` (username, encrypted_password, created_at, updated_at)" +
+            "VALUES (#{username}, #{encryptedPassword}, now(), now())")
+    void save(@Param("username") String username, @Param("encryptedPassword") String encryptedPassword);
+
+    @Select("select * from user where username = #{username}")
+    User findUserByUsername(@Param("username") String username);
 }
--- src/main/java/cn/wbomb/www/service/UserService.java
+++ src/main/java/cn/wbomb/www/service/UserService.java
@@ -1,6 +1,7 @@
 package cn.wbomb.www.service;
 
 import cn.wbomb.www.entity.User;
+import cn.wbomb.www.mapper.UserMapper;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -9,36 +10,28 @@ import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
 import java.util.Collections;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class UserService implements UserDetailsService {
 
-    //    private UserMapper userMapper;
+    private final UserMapper userMapper;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
-    private final Map<String, User> users = new ConcurrentHashMap<>();
 
     @Inject
-    public UserService(BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public UserService(BCryptPasswordEncoder bCryptPasswordEncoder, UserMapper userMapper) {
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-        save("tmz", "123456");
+        this.userMapper = userMapper;
     }
 
     public void save(String username, String password) {
-        users.put(username,
-                new User(1, username, bCryptPasswordEncoder.encode(password)));
+        userMapper.save(username, bCryptPasswordEncoder.encode(password));
     }
 
 
     public User getUserByUsername(String username) {
-        return users.get(username);
+        return userMapper.findUserByUsername(username);
     }
 
-//    @Inject
-//    public UserService(UserMapper userMapper) {
-//        this.userMapper = userMapper;
-//    }
 
 //    public User getUserById(int id) {
 //        return userMapper.getUserById(id);
@@ -46,11 +39,10 @@ public class UserService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if (!users.containsKey(username)) {
+        User user = userMapper.findUserByUsername(username);
+        if (user == null) {
             throw new UsernameNotFoundException(username + "不存在");
         }
-        User user = users.get(username);
-
         return new org.springframework.security.core.userdetails.User(username, user.getEncryptedPassword(), Collections.emptyList());
     }
 }
