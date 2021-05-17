17ccb72b682f28cd2f31eb48ca76eeab6c306cb8
George <jobsteng@163.com>
2021-05-17 23:23:06 +0800

    feat(test):Test AuthController unit test

--- src/test/java/cn/wbomb/www/controller/AuthControllerTest.java
+++ src/test/java/cn/wbomb/www/controller/AuthControllerTest.java
@@ -1,20 +1,34 @@
 package cn.wbomb.www.controller;
 
 import cn.wbomb.www.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.security.authentication.AuthenticationManager;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import javax.servlet.http.HttpSession;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,6 +39,8 @@ class AuthControllerTest {
     private UserService userService;
     @Mock
     private AuthenticationManager authenticationManager;
+
+    private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 
     @BeforeEach
     void setUp() {
@@ -37,5 +53,29 @@ class AuthControllerTest {
     void returnNotLoginByDefault() throws Exception {
         mvc.perform(get("/auth")).andExpect(status().isOk())
                 .andExpect(mvcResult -> Assertions.assertTrue(mvcResult.getResponse().getContentAsString().contains("ok")));
+    }
+
+    @Test
+    void testLogin() throws Exception {
+        mvc.perform(get("/auth")).andExpect(status().isOk())
+                .andExpect(mvcResult -> Assertions.assertTrue(mvcResult.getResponse().getContentAsString().contains("用户没有登录")));
+
+        Map<String, String> usernamePassword = new HashMap<>();
+        usernamePassword.put("username", "MyUser");
+        usernamePassword.put("password", "MyPassword");
+
+        Mockito.when(userService.loadUserByUsername("MyUser")).thenReturn(new User("MyUser", bCryptPasswordEncoder.encode("MyPassword"), Collections.emptyList()));
+
+        MvcResult response = mvc.perform(post("/auth/login").contentType(
+                MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(usernamePassword)))
+                .andExpect(status().isOk())
+                .andExpect(mvcResult -> Assertions.assertTrue(mvcResult.getResponse().getContentAsString().contains("登录成功")))
+                .andReturn();
+        HttpSession session = response.getRequest().getSession();
+        mvc.perform(get("/auth").session((MockHttpSession) session)).andExpect(status().isOk())
+                .andExpect(mvcResult -> Assertions.assertTrue(mvcResult.getResponse()
+                        .getContentAsString().contains("MyUser")));
+
     }
 }
