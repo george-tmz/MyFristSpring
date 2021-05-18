1b70de207ec2a2ed11ed4d438000377ca3834da7
George <jobsteng@163.com>
2021-05-18 22:30:17 +0800

    编写集成测试

---
+++ src/test/java/cn/wbomb/www/integration/MyIntegrationTest.java
@@ -0,0 +1,18 @@
+package cn.wbomb.www.integration;
+
+import cn.wbomb.www.Application;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:test.properties")
+public class MyIntegrationTest {
+    @Test
+    public void indexHtmlIsAccessible() {
+
+    }
+}
