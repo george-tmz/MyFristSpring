fdcf5b5d1ef8982ccfa18537a49131f0ee2db5b4
George <jobsteng@163.com>
2021-05-19 00:04:53 +0800

    编写集成测试

--- src/test/java/cn/wbomb/www/integration/MyIntegrationTest.java
+++ src/test/java/cn/wbomb/www/integration/MyIntegrationTest.java
@@ -4,15 +4,22 @@ import cn.wbomb.www.Application;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.inject.Inject;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(locations = "classpath:test.properties")
 public class MyIntegrationTest {
+    @Inject
+    Environment environment;
+
     @Test
     public void indexHtmlIsAccessible() {
+        System.out.println(environment.getProperty("local.server.port"));
 
     }
 }
